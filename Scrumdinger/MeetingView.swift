aa8d0709435f35ed48dc2792d4a47f0a14b22711
Sanghun <po4865@gmail.com>
2022-10-16 20:32:51 +0200

    feat: Start development
    - Change the name of file and struct to `MeetingView`.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MeetingView.swift
 //  Scrumdinger
 //
 //  Created by Sanghun Park on 15.10.22.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MeetingView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -19,8 +19,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MeetingView()
     }
 }
