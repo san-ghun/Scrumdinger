c1675804609fded48839e5af8f4048068ffe5ded
Sanghun <po4865@gmail.com>
2022-10-16 20:24:37 +0200

    init: Initiate project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Sanghun Park on 15.10.22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
