c56f6bb014c81ea734c81738e71ecf9b82e8b64f
Sanghun <po4865@gmail.com>
2022-11-14 20:35:54 +0100

    feat: Create a data store
    - Create a new class to serve asa the data model for the app.
    
    ScrumStore.swift
    - Create a new file `ScrumStore.swift`.
    - Import SwiftUI, then create a ScrumStore class that conforms to ObservableObject, which is a class-constrained protocol for connecting external model data to SwiftUI views.
    - 12: Add a @Published `scrums` property of type `[DailyScrum]`.
    	- An ObservableObject includes an objectWillChange publisher that emits when one of its @Published properties is about to change. Any view observing an instance of ScrumStore will render again when the scrums value changes.
    - 14: Add a private static throwing function `fileURL` that returns a URL.
    - 15: Call `url(...)` on the default file manager, which use the shared instance of the FileManger class to get the location of the Documents directory for the current user.
    - 16: Call `appendingPathComponent()` to return the URL of a file `scrums.data`.

---
+++ Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Sanghun Park on 14.11.22.
+//
+
+import Foundation
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("scrums.data")
+    }
+}
