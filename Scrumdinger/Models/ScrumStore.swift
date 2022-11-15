36abd1b2e448d757f246744ee71678766e744b43
Sanghun <po4865@gmail.com>
2022-11-15 21:59:38 +0100

    feat: Create an asynchronous load method
    ScrumStore.swift
    - 19: Create a staic function `load` that returns an array of scrums.
    - 20: Call `withCheckedThrowingContinuation` using the await keyword.
    	- Calling the function suspends the `load` function, then passes a continuation into a closure that provided. A continuation is a value that represents the code after an awaited function.
    - 21: In the closure, call the legacy `load` function with a completion handler.
    - 22: Add a switch statement to handle the `result` cases.
    	- Upon success, send the scrums to the continuation closure. The array of scrums becomes the result of the `withCheckedThrowingContinuation` call when the async task resumes.
    	- Upon failure, send the error to the continuation closure. The system throws an error when the async task resumes.

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -16,6 +16,19 @@ class ScrumStore: ObservableObject {
             .appendingPathComponent("scrums.data")
     }
     
+    static func load() async throws -> [DailyScrum] {
+        try await withCheckedThrowingContinuation { continuation in
+            load { result in
+                switch result {
+                case .success(let scrums):
+                    continuation.resume(returning: scrums)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+    
     static func load(completion: @escaping (Result<[DailyScrum], Error>)->Void) {
         DispatchQueue.global(qos: .background).async {
             do {
