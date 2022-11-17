cf3ca1f40cfbc27536e92507a545ea171b74e10c
Sanghun <po4865@gmail.com>
2022-11-17 15:34:52 +0100

    feat: Use tasks to call asynchronous methods
    ScrumStore.swift
    - Reorganize the order of method in the file.
    - Make `load()` and `save()` methods with escaping closure to private methods.
    
    ScrumdingerApp.swift
    - 18: Replace the call to `ScrumStore.save()` in the list view's `saveAction` closure with a new `Task`.
    	- `Task` creates a new asynchronous context.
    - 19-24: Add a do-catch statement.
    	- In do clause, call `ScrumStore.save()`.
    	- In catch clause, call `fatalError()`.
    - 28: Replace the `.onAppear` modifer with a `.task` modifier.
    - 29-34: Add a do-catch statement.
    	- In do clause, call `ScrumStore.load()`. Set `scrums` to the value that the function returns.
    	- In catch clause, call `fatalError()`.

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -16,6 +16,29 @@ class ScrumStore: ObservableObject {
             .appendingPathComponent("scrums.data")
     }
     
+    private static func load(completion: @escaping (Result<[DailyScrum], Error>)->Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let fileURL = try fileURL()
+                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
+                    DispatchQueue.main.async {
+                        completion(.success([]))
+                    }
+                    return
+                }
+                let dailyScrums = try JSONDecoder().decode([DailyScrum].self, from: file.availableData)
+                DispatchQueue.main.async {
+                    completion(.success(dailyScrums))
+                }
+            }
+            catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
     static func load() async throws -> [DailyScrum] {
         try await withCheckedThrowingContinuation { continuation in
             load { result in
@@ -29,19 +52,14 @@ class ScrumStore: ObservableObject {
         }
     }
     
-    static func load(completion: @escaping (Result<[DailyScrum], Error>)->Void) {
+    private static func save(scrums: [DailyScrum], completion: @escaping (Result<Int, Error>)->Void) {
         DispatchQueue.global(qos: .background).async {
             do {
-                let fileURL = try fileURL()
-                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
-                    DispatchQueue.main.async {
-                        completion(.success([]))
-                    }
-                    return
-                }
-                let dailyScrums = try JSONDecoder().decode([DailyScrum].self, from: file.availableData)
+                let data = try JSONEncoder().encode(scrums)
+                let outfile = try fileURL()
+                try data.write(to: outfile)
                 DispatchQueue.main.async {
-                    completion(.success(dailyScrums))
+                    completion(.success(scrums.count))
                 }
             }
             catch {
@@ -66,21 +84,4 @@ class ScrumStore: ObservableObject {
         }
     }
     
-    static func save(scrums: [DailyScrum], completion: @escaping (Result<Int, Error>)->Void) {
-        DispatchQueue.global(qos: .background).async {
-            do {
-                let data = try JSONEncoder().encode(scrums)
-                let outfile = try fileURL()
-                try data.write(to: outfile)
-                DispatchQueue.main.async {
-                    completion(.success(scrums.count))
-                }
-            }
-            catch {
-                DispatchQueue.main.async {
-                    completion(.failure(error))
-                }
-            }
-        }
-    }
 }
