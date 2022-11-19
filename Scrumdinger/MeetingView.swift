7637f2f96daf2a3326219158ba84e1f8120f21f5
Sanghun <po4865@gmail.com>
2022-11-19 16:25:46 +0100

    feat: Create a history view
    Create a history view that displays the meeting text and attendees.
    
    History.swift
    - Add a `transcript` property that’s an optional string. Update the initializer with the new property and a default value of nil.
    
    MeetingView.swift
    - Initialize History with the `speechRecognizer.transcript` property.
    
    HistoryView.swift
    - Create a SwiftUI View file, and add a `history` constant.
    - In the preview provider, add a `history` variable that contains sample data. Then pass the variable to the `HistoryView()` initializer.
    - 14-15: Create a VStack with leading alignment and embed it in a scroll view.
    - 16-17: Add a divider with bottom padding to create visual separation between the header and the content.
    - 18-19: Create an Attendees headline.
    - 20,34-38: Add a `History` extension with a list formatter to display attendees in a human-readable string. Then add a text view, and pass in the meeting attendees.
    - 21-26: Use optional binding to safely unwrap the value of `transcript`. If a transcript has a value, display "Transcript," followed by the transcript text. And modify the Transcript label with the headline font style and top padding.
    - 29-30: Add a navigation title to the scroll view that shows `history.date` with a `.date` style. Add padding after the title.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -43,7 +43,7 @@ struct MeetingView: View {
             scrumTimer.stopScrum()
             speechRecognizer.stopTranscribing()
             isRecoding = false
-            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60)
+            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60, transcript: speechRecognizer.transcript)
             scrum.history.insert(newHistory, at: 0)
         }
         .navigationBarTitleDisplayMode(.inline)
