98055b316808d97d71238b81fddb6d614d8fef5b
Sanghun <po4865@gmail.com>
2022-11-19 15:10:28 +0100

    feat: Integrate speech recognition
    Integrate the speeh recognizer into the meeting view by calling methods at specific points in the view's life cycle.
    
    MeetingView.swift
    - 14: Create a new state object `speechRecognizer`. The initializer requests access to the speech recognizer and microphone the first time the system calls the object.
    - 15: Add a @State property for `isRecording`, to display recording indicators later.
    - 35,36: Call `speechRecognizer.reset()` and `speechRecognizer.transcribe()`.
    - 37: Set the value of `isRecording` to true.
    - 39: Call `speechRecognizer.stopTranscribing()`.
    - 40: Set the value of `isRecording` to false.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -11,6 +11,8 @@ import AVFoundation
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecoding = false
     
     private var player: AVPlayer { AVPlayer.sharedDingPlayer }
     
@@ -32,10 +34,15 @@ struct MeetingView: View {
                 player.seek(to: .zero)
                 player.play()
             }
+            speechRecognizer.reset()
+            speechRecognizer.transcribe()
+            isRecoding = true
             scrumTimer.startScrum()
         }
         .onDisappear {
             scrumTimer.stopScrum()
+            speechRecognizer.stopTranscribing()
+            isRecoding = false
             let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrum.timer.secondsElapsed / 60)
             scrum.history.insert(newHistory, at: 0)
         }
